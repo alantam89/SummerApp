5204e7ec797a8c4d07ab0242fb1b20fd36f7412e
cdelp <cdelp@hawaii.edu>
2016-03-06 23:40:38 -1000

    Unsucessfully attampted to add map. Project filled with error messages due to version change.

--- Application/src/main/java/com/example/android/horizontalpaging/MainActivity.java
+++ Application/src/main/java/com/example/android/horizontalpaging/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
 import android.widget.ImageView;
+import android.widget.FrameLayout;
 
 import java.util.Locale;
 
